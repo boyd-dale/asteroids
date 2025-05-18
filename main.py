7f8b74bdac2fdbdc1028724ca1e4d0c9ec3febcb
boyd-dale <boydr81@gmail.com>
2025-05-18 20:55:59 +1000

    Setup window with close

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while(True):
+        
+        # Return if the user has closed the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+        # Fill the screen black
+        screen.fill(0)
+
+
+
+        # Refresh the screen, must be called last
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
