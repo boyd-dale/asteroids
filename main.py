8083ca4e7fdf1684e9b4c6b0ead25b964dc71154
boyd-dale <boydr81@gmail.com>
2025-05-18 21:01:52 +1000

    Setup FPS limits

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Init pygame
     pygame.init()
 
+    # Init screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Init clock
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while(True):
         
@@ -25,6 +32,9 @@ def main():
 
         # Refresh the screen, must be called last
         pygame.display.flip()
+
+        # Pause the game loop until 1/60th of a second has passed
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
